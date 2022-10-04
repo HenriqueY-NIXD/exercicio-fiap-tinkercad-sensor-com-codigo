5301eb1ef7f9686425f890102696f8664b7dae77
Henrique Yamada <henriqueyamada2004@gmail.com>
2022-10-04 15:46:54 -0300

    Fix main.cpp

--- main.cpp
+++ main.cpp
@@ -38,6 +38,7 @@ int index_linha = 1;
 int is_resetar_senha = 0;
 int is_senha_atual_certa = 0;
 int is_intruder_alert = 0;
+int is_entry_permitted = 0;
 
 unsigned long lastDebounceTime = 0;
 
@@ -55,129 +56,83 @@ void setup()
   Serial.begin(9600);
   Wire.begin();
   
-  // pinMode(BUZZER, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
   pinMode(LUZ_VERMELHA, OUTPUT);
   pinMode(LUZ_VERDE, OUTPUT);
   pinMode(PIN_SENSOR, INPUT);
   
   lcd.begin(16, 2);
-  /*
-  lcd.print("HELLO WORLD!");
-  digitalWrite(11, HIGH);
-  delay(250);
-  digitalWrite(11, LOW);
-  delay(250);
-  digitalWrite(11, HIGH);
-  delay(250);
-  digitalWrite(11, LOW);
-  
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("FIAP");
-  lcd.setCursor(0, 1);
-  lcd.print("CYBER SEGURANCA");
-  delay(1000);
-  
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("INTEGRANTES:");
-  delay(1000);
-  
-  lcd.clear();
-  
-  lcd.setCursor(0, 0);
-  lcd.print("ANDERSON CARLOS");
-  lcd.setCursor(0, 1);
-  lcd.print("94982");
-  delay(1000);
-  
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("HENRIQUE YAMADA");
-  lcd.setCursor(0, 1);
-  lcd.print("95863");
-  delay(1000);
-  
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("ISABELA SANTANA");
-  lcd.setCursor(0, 1);
-  lcd.print("96066");
-  delay(1000);
-  
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("WALLACE SAMPAIO");
-  lcd.setCursor(0, 1);
-  lcd.print("95958");
-  delay(1000);
-  
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  
-  for(int i = 0; i < 3; i++) {
-    lcd.setCursor(0, 0);
-  	lcd.print("CARREGADO.");
-    delay(500);
-    lcd.setCursor(0, 0);
-    lcd.print("CARREGADO..");
-    delay(500);
-    lcd.setCursor(0, 0);
-    lcd.print("CARREGADO...");
-    delay(500);
-    lcd.clear();
-  }
-
-  lcd.setCursor(0, 0);
-  digitalWrite(LUZ_VERDE, HIGH);
-  delay(250);
-  digitalWrite(LUZ_VERDE, LOW);
-  delay(250);
-  digitalWrite(LUZ_VERDE, HIGH);
-  delay(250);
-  digitalWrite(LUZ_VERDE, LOW);*/
-
   lcd.setCursor(0, 0);
   printarMensagem();
 }
 
 void loop()
 {
-  if (is_intruder_alert == 1)
-    tone(BUZZER, DOS);
-
-  int current_time = millis();
-	lcd.setCursor(index_coluna, index_linha);
-	char tecla_apertada = keypad.getKey();
-
   int sensor_pin_status = digitalRead(PIN_SENSOR);
-  
-  // if (is_intruder_alert == 1 && is_resetar_senha == 1 || is_senha_atual_certa == 1) {
-  //   lcd.clear();
-  //   zerarSenha();
-  //   is_resetar_senha = 0;
-  //   is_senha_atual_certa = 0;
-  //   lcd.setCursor(0, 0);
-    
-  //   printarMensagem();
-
-  //   index_coluna = 0;
-  // }
 
   if(sensor_pin_status == HIGH)
     is_intruder_alert = 1;
 
-	if (tecla_apertada)
-	{
-		int is_tecla_apertada_number = 0;
+  if (is_intruder_alert == 1 && is_entry_permitted == 0)
+    tone(BUZZER, DOS);
+
+  int current_time = millis();
+  char tecla_apertada = keypad.getKey();
+
+  if (tecla_apertada) {
+    int is_tecla_apertada_number = 1;
 		for(int i = 0; i < 4; i++) {
 			if (tecla_apertada == teclas_letras[i]) {
-				is_tecla_apertada_number = 1;
+				is_tecla_apertada_number = 0;
 				break;
 			}
 		}
-		if(is_tecla_apertada_number == 0){
-			if (tecla_apertada == '#'){
+
+    if (is_tecla_apertada_number == 0) {
+      switch(tecla_apertada){
+				case 'A':
+          is_entry_permitted = 0;
+          is_resetar_senha = 0;
+          is_senha_atual_certa = 0;
+          index_coluna = 0;
+          zerarSenha();
+          lcd.clear();
+          lcd.setCursor(0, 0);
+          printarMensagem();
+					break;
+				case 'B':
+          if (is_intruder_alert == 0 || is_entry_permitted == 1) {
+            if (is_resetar_senha == 0) {
+              is_resetar_senha = 1;
+            } else {
+              is_resetar_senha = 0;
+              is_senha_atual_certa = 0;
+            }
+            index_coluna = 0;
+            zerarSenha();
+            lcd.clear();
+            lcd.setCursor(0, 0);
+            printarMensagem();
+          }
+					break;
+				case 'C':
+				case 'D':
+          is_entry_permitted = 0;
+          is_resetar_senha = 0;
+          is_senha_atual_certa = 0;
+          is_intruder_alert = 1;
+          index_coluna = 0;
+          zerarSenha();
+          lcd.clear();
+          lcd.setCursor(0, 0);
+          printarMensagem();
+					break;
+				default:
+					Serial.println(tecla_apertada);
+					break;
+			}
+    } else if((is_resetar_senha == 1 || is_senha_atual_certa == 1) && is_entry_permitted == 1 || is_entry_permitted == 0){
+      if (tecla_apertada == '#'){
 				if (index_coluna != 0){
 					lcd.clear();
           lcd.setCursor(0, 0);
@@ -198,139 +153,51 @@ void loop()
         printarMensagem();
 
 				index_coluna = 0;
-			} else {
-				lcd.print(tecla_apertada);
+			} else{
+        lcd.setCursor(index_coluna, index_linha);
+        lcd.print(tecla_apertada);
 				senha_escrita[index_coluna] = tecla_apertada;
 
-				if(index_coluna == 5){
-					Serial.println("");
-					for(int i=0; i<6; i++) {
-						Serial.print(senha_escrita[i]);
-					}
-					Serial.println("");
-
-					delay(500);
-
-					lcd.clear();
+        if (index_coluna == 5) {
+          lcd.clear();
 					lcd.setCursor(0, 0);
-					
           if (is_resetar_senha == 1 && is_senha_atual_certa == 1) {
             for(int i=0; i<6; i++) {
               senha[i] = senha_escrita[i];
             }
             is_resetar_senha = 0;
             is_senha_atual_certa = 0;
-            index_coluna = 0;
-            zerarSenha();
-            lcd.clear();
-            lcd.setCursor(0, 0);
-            printarMensagem();
-          } else if (!(strncmp(senha_escrita, senha, 6))){
-						if (is_resetar_senha == 0) {
-              lcd.print("SENHA CERTA");
-              Serial.println("Senha Certa");
-              digitalWrite(LUZ_VERDE, HIGH);
-              // tone(BUZZER, DOS, 250);
-              is_intruder_alert = 0;
-              Wire.beginTransmission(slaveAdress);
-              Wire.write(LOW);
-              Wire.endTransmission();
-              delay(100);
-              digitalWrite(LUZ_VERDE, LOW);
-              delay(100);
-              digitalWrite(LUZ_VERDE, HIGH);
-              // tone(BUZZER, DOS, 250);
-              delay(100);
-              digitalWrite(LUZ_VERDE, LOW);
-            } else {
+          } else if(!(strncmp(senha_escrita, senha, 6))) {
+            if (is_resetar_senha == 1) {
               is_senha_atual_certa = 1;
-            }
-					} else {
-						lcd.print("SENHA ERRADA");
-						digitalWrite(LUZ_VERMELHA, HIGH);
-						// tone(BUZZER, DO, 250);
-						delay(250);
-						digitalWrite(LUZ_VERMELHA, LOW);
-						delay(250);
-						digitalWrite(LUZ_VERMELHA, HIGH);
-						// tone(BUZZER, DO, 250);
-						delay(250);
-						digitalWrite(LUZ_VERMELHA, LOW);
-					}
+            } else {
+              lcd.print("SENHA CERTA");
+              noTone(BUZZER);
+              
+              is_intruder_alert = 0;
+              is_entry_permitted = 1;
 
+              digitalWrite(LUZ_VERDE, HIGH);
+              delay(100);
+              digitalWrite(LUZ_VERDE, LOW);
+              delay(100);
+              digitalWrite(LUZ_VERDE, HIGH);
+              delay(100);
+              digitalWrite(LUZ_VERDE, LOW);
+              delay(500);
+            }
+          }
           index_coluna = 0;
-					zerarSenha();
-					lcd.clear();
+          zerarSenha();
+          lcd.clear();
           lcd.setCursor(0, 0);
           printarMensagem();
-				} else {
-					++index_coluna;
-				}
-			}
-		} else if (index_coluna == 0 ) {
-			switch(tecla_apertada){
-				case 'A':
-          lcd.clear();
-          
-          lcd.setCursor(0, 0);
-					lcd.print("ANDERSON CARLOS");
-					lcd.setCursor(0, 1);
-					lcd.print("94982");
-					delay(1000);
-					lcd.clear();
-
-          lcd.setCursor(0, 0);
-          lcd.print("HENRIQUE YAMADA");
-					lcd.setCursor(0, 1);
-					lcd.print("95863");
-					delay(1000);
-					lcd.clear();
-
-          lcd.setCursor(0, 0);
-          lcd.print("ISABELA SANTANA");
-					lcd.setCursor(0, 1);
-					lcd.print("96066");
-					delay(1000);
-					lcd.clear();
-
-          lcd.setCursor(0, 0);
-          lcd.print("WALLACE SAMPAIO");
-					lcd.setCursor(0, 1);
-					lcd.print("95958");
-					delay(1000);
-					lcd.clear();
-					break;
-				case 'B':
-          if (is_resetar_senha == 0 && is_intruder_alert == 0) {
-            is_resetar_senha = 1;
-            lcd.clear();
-            lcd.setCursor(0, 0);
-            printarMensagem();
-            lcd.setCursor(0, 1);
-          } else {
-            is_resetar_senha = 0;
-            is_senha_atual_certa = 0;
-            lcd.clear();
-            lcd.setCursor(0, 0);
-            zerarSenha();
-            printarMensagem();
-          }
-					break;
-				case 'C':
-          buzzer_state = !buzzer_state;
-					break;
-				case 'D':
-					break;
-				default:
-          lcd.clear();
-          lcd.setCursor(0, 0);
-					lcd.print("??????????");
-					delay(1000);
-					Serial.println(tecla_apertada);
-					break;
-			}
+        } else {
+          ++index_coluna;
+        }
+      }
 		}
-	}
+  }
 }
 
 void zerarSenha() {
@@ -340,8 +207,12 @@ void zerarSenha() {
 }
 
 void printarMensagem() {
-  if (is_resetar_senha == 0)
-    _initialMessage();
+  if (is_resetar_senha == 0){
+    if (is_entry_permitted == 1)
+      _entryPermitedMessage();
+    else
+      _initialMessage();
+  }
   else {
     if (is_senha_atual_certa == 0)
       _resetPasswordMessage();
@@ -360,6 +231,10 @@ void _resetPasswordMessage() {
 
 void _newPasswordMessage() {
   lcd.print("NOVA SENHA:");
+}
+
+void _entryPermitedMessage() {
+  lcd.print("ACESSO LIBERADO");
 }
 
 
